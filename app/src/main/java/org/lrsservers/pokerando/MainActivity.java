7de9641bd12e15f50a6dc94c2a3c4a4542fb9e4a
Larson Thorpe (Lrs121) <ltthorpe@lrsservers.org>
2021-10-08 23:59:20 -0700

    finish baisc interface

--- app/src/main/java/org/lrsservers/pokerando/MainActivity.java
+++ app/src/main/java/org/lrsservers/pokerando/MainActivity.java
@@ -1,8 +1,8 @@
 package org.lrsservers.pokerando;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
