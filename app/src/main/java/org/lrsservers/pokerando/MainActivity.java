ae49170473c8e71ed6ab5563811949d3d802dbb3
Larson Thorpe (Lrs121) <ltthorpe@lrsservers.org>
2021-10-18 16:31:19 -0700

    more work on interface bringup'

--- app/src/main/java/org/lrsservers/pokerando/MainActivity.java
+++ app/src/main/java/org/lrsservers/pokerando/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,6 +25,8 @@ import com.anggrayudi.storage.permission.ActivityPermissionRequest;
 import com.anggrayudi.storage.permission.PermissionCallback;
 import com.anggrayudi.storage.permission.PermissionReport;
 import com.anggrayudi.storage.permission.PermissionResult;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.jetbrains.annotations.NotNull;
 import org.lrsservers.pokerando.upr.RandomSource;
@@ -35,26 +39,28 @@ import org.lrsservers.pokerando.upr.romhandlers.Gen4RomHandler;
 import org.lrsservers.pokerando.upr.romhandlers.Gen5RomHandler;
 import org.lrsservers.pokerando.upr.romhandlers.RomHandler;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final SimpleStorageHelper storageHelper = new SimpleStorageHelper(this);
     private final int SAVE_ROM = 10, LOAD_ROM = 15;
-    private final ActivityPermissionRequest permissionRequest = new ActivityPermissionRequest.Builder(this)
-                  .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-                  .withCallback(new PermissionCallback() {
-                       @Override
-                       public void onPermissionsChecked(@NotNull PermissionResult result, boolean fromSystemDialog) {
-                       }
-                       @RequiresApi(api = Build.VERSION_CODES.R)
-                       @Override
-                       public void onShouldRedirectToSystemSettings(@NotNull List<PermissionReport> blockedPermissions) {
-                             storageHelper.requestStorageAccess();
-                       }
-                  }).build();
+    private final ActivityPermissionRequest permissionRequest= new ActivityPermissionRequest.Builder(this)
+                                                                        .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+                                                                        .withCallback(new PermissionCallback() {
+                                                                            @Override
+                                                                            public void onPermissionsChecked(@NotNull PermissionResult result, boolean fromSystemDialog) {
+                                                                            }
+
+                                                                            @RequiresApi(api = Build.VERSION_CODES.R)
+                                                                            @Override
+                                                                            public void onShouldRedirectToSystemSettings(@NotNull List<PermissionReport> blockedPermissions) {
+                                                                                storageHelper.requestStorageAccess();
+                                                                            }
+                                                                        }).build();
+    private final GenRestrictions genRestrictions = null;
     private ImageButton ibLoadRom, ibSaveRom;
     private RomHandler romHandler = null;
-    private final GenRestrictions genRestrictions = null;
     private RomHandler.Factory[] factories;
 
     @Override
@@ -70,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setupSimpleStorage(savedInstanceState);
         setupBtnAction();
+        initialState();
 
     }
 
@@ -121,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Utils.validateRomFile(DocumentFileUtils.toRawFile(files.get(0), getApplicationContext()));
             } catch (Utils.InvalidROMException e) {
-                switch (e.getType()){
+                switch (e.getType()) {
                     case LENGTH:
                         Log.e(String.valueOf(R.string.invalid_rom), String.valueOf(R.string.rom_too_short));
                         Toast.makeText(MainActivity.this, String.valueOf(R.string.rom_too_short), Toast.LENGTH_SHORT).show();
@@ -144,10 +151,22 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                         break;
                     default:
-                        for(RomHandler.Factory factory : factories){
-                            if (factory.isLoadable( DocumentFileUtils.getBaseName(files.get(0)))){
+                        for (RomHandler.Factory factory : factories) {
+                            if (factory.isLoadable(DocumentFileUtils.getBaseName(files.get(0)))) {
                                 this.romHandler = factory.create(RandomSource.instance());
+                                TextView romName = findViewById(R.id.txtRomName);
+                                TextView romCode = findViewById(R.id.txtRomCode);
+                                TextView romFile = findViewById(R.id.txtFileName);
+                                TextView supportStat = findViewById(R.id.txtRomSupport);
+                                romFile.setText(DocumentFileUtils.getBaseName(files.get(0)));
+                                romName.setText(this.romHandler.getROMName());
+                                romCode.setText(this.romHandler.getROMCode());
+                                supportStat.setText(this.romHandler.getSupportLevel());
+                                enableUI(DocumentFileUtils.toRawFile(files.get(0), getApplicationContext()));
 
+
+                            }else{
+                                Snackbar.make(MainActivity.this, findViewById(R.id.topLayout), String.valueOf(R.string.invalid_rom), Snackbar.LENGTH_SHORT).show();
                             }
                         }
                         break;
@@ -159,17 +178,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void initialState(){
-//        SwitchMaterial swRace, swLimitPkm, swGameBreakingMoves, swStdExpCurve, swFollowEvo, swUpdateBaseStats, swAllwo;
-        Button btnSelectPkm = findViewById(R.id.btnLimitPoke);
+    private void initialState() {
         ScrollView mainView = findViewById(R.id.clMain);
+        RadioGroup rgBase = findViewById(R.id.rbgpBaseStats), rgAbility = findViewById(R.id.rgPokeAbility), rgTypes = findViewById(R.id.rgPokeTypes), rgEvo = findViewById(R.id.rgPokeEvoRand), rgStarter = findViewById(R.id.rgStarterPoke), rgMovesets = findViewById(R.id.rgMoveSets), rgTrainerRand = findViewById(R.id.rgTrainerRand), rgWildsPoke = findViewById(R.id.rgWildsRandSetting), rgWildsRules = findViewById(R.id.rgWildsAddRules), rgTMRand = findViewById(R.id.rgTMRando), rgTMHMComp = findViewById(R.id.rgTMHMCompats), rgTutorMoves = findViewById(R.id.rgTutorMoveRand), rgTutorCompat = findViewById(R.id.rgTutorCompat), rgTrades = findViewById(R.id.rgTradeRandom), rgFieldItems = findViewById(R.id.rgFieldItemsRand);
+        RadioGroup[] radioGroups = new RadioGroup[]{rgBase, rgAbility, rgTypes, rgEvo, rgStarter, rgMovesets, rgTrainerRand, rgWildsPoke, rgWildsRules, rgTMRand, rgTMHMComp, rgTutorCompat, rgTutorMoves, rgTrades, rgFieldItems};
+        ImageView boxArt = findViewById(R.id.imgBoxArt);
 
-        btnSelectPkm.setEnabled(false);
-        for(int i = 0; i < mainView.getChildCount(); i++){
+        for (int i = 0; i < mainView.getChildCount(); i++) {
             View child = mainView.getChildAt(i);
             child.setEnabled(false);
         }
+        for (RadioGroup radioGroup : radioGroups) {
+            for (int j = 0; j < radioGroup.getChildCount(); j++) {
+                View child = radioGroup.getChildAt(j);
+                child.setEnabled(false);
+            }
+        }
+        boxArt.setVisibility(View.INVISIBLE);
+    }
 
+    private void enableUI(){
+        ScrollView mainView = findViewById(R.id.clMain);
+        RadioGroup rgBase = findViewById(R.id.rbgpBaseStats), rgAbility = findViewById(R.id.rgPokeAbility), rgTypes = findViewById(R.id.rgPokeTypes), rgEvo = findViewById(R.id.rgPokeEvoRand), rgStarter = findViewById(R.id.rgStarterPoke), rgMovesets = findViewById(R.id.rgMoveSets), rgTrainerRand = findViewById(R.id.rgTrainerRand), rgWildsPoke = findViewById(R.id.rgWildsRandSetting), rgWildsRules = findViewById(R.id.rgWildsAddRules), rgTMRand = findViewById(R.id.rgTMRando), rgTMHMComp = findViewById(R.id.rgTMHMCompats), rgTutorMoves = findViewById(R.id.rgTutorMoveRand), rgTutorCompat = findViewById(R.id.rgTutorCompat), rgTrades = findViewById(R.id.rgTradeRandom), rgFieldItems = findViewById(R.id.rgFieldItemsRand);
+        RadioGroup[] radioGroups = new RadioGroup[]{rgBase, rgAbility, rgTypes, rgEvo, rgStarter, rgMovesets, rgTrainerRand, rgWildsPoke, rgWildsRules, rgTMRand, rgTMHMComp, rgTutorCompat, rgTutorMoves, rgTrades, rgFieldItems};
+        ImageView boxArt = findViewById(R.id.imgBoxArt);
+
+        for (int i = 0; i < mainView.getChildCount(); i++) {
+            View child = mainView.getChildAt(i);
+            child.setEnabled(true);
+        }
+        boxArt.setEnabled(false);
+        for (RadioGroup radioGroup : radioGroups) {
+            for (int j = 0; j < radioGroup.getChildCount(); j++) {
+                View child = radioGroup.getChildAt(j);
+                child.setEnabled(true);
+            }
+        }
+        if(this.romHandler.generationOfPokemon() < 6){
+
+        }
+    }
+
+    private void setBoxArt(String romCode){
+        ImageView boxArt = findViewById(R.id.imgBoxArt);
+        switch (romCode){
+            case "BPRE0":
+                break;
+            default:
+                break;
+        }
     }
 
 }
